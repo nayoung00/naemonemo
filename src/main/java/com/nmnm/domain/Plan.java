e2e54fa40e9c54f699109f38412478ec8dce96c5
sun-kyung <esun1801@naver.com>
2020-04-06 19:39:24 +0900

    [add] new class

---
+++ src/main/java/com/nmnm/domain/Plan.java
@@ -0,0 +1,16 @@
+package com.nmnm.domain;
+
+import java.util.Date;
+
+public class Plan {
+  private Date date;
+  private String title;
+  private String placeName;
+  private String adress;
+  private int latitude;
+  private int longitude;
+
+  public void planCalender(){};
+  public void planRoute(){};
+  public void planBoard(){};
+}

46392f55437c988b7cd4b9919bbcc381d751fa0b
nayoung00 <invin1201@gmail.com>
2020-04-09 17:36:14 +0900

    [add] dto 추가
    Grade dto, Group dto, GroupMember dto, Member dto 추가

---
+++ src/main/java/com/nmnm/dto/GroupMember.java
@@ -0,0 +1,48 @@
+package com.nmnm.dto;
+
+public class GroupMember {
+
+  private int groupNo; // pk, fk일정참여자,피드, 공지사항
+  private int memberNo; // pk, fk일정참여자, 피드, 공지사항
+  private int gradeNo; // fk등급
+
+
+  @Override
+  public String toString() {
+    return "GroupMember [groupNo=" + groupNo + ", memberNo=" + memberNo + ", gradeNo=" + gradeNo
+        + "]";
+  }
+
+
+  public int getGroupNo() {
+    return groupNo;
+  }
+
+
+  public void setGroupNo(int groupNo) {
+    this.groupNo = groupNo;
+  }
+
+
+  public int getMemberNo() {
+    return memberNo;
+  }
+
+
+  public void setMemberNo(int memberNo) {
+    this.memberNo = memberNo;
+  }
+
+
+  public int getGradeNo() {
+    return gradeNo;
+  }
+
+
+  public void setGradeNo(int gradeNo) {
+    this.gradeNo = gradeNo;
+  }
+
+
+
+}

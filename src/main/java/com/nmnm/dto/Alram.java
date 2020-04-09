9a563595103a5375286eff800781064878b384b8
jyson19 <jyson19@gmail.com>
2020-04-09 17:37:03 +0900

    [add] 회계 관련 및 알람, 메시지 DTO 추가

---
+++ src/main/java/com/nmnm/dto/Alram.java
@@ -0,0 +1,41 @@
+package com.nmnm.dto;
+
+public class Alram {
+  private int memberNo; // pk(fk from 회원)
+  private int noticeBoardNo; // fk from 회원
+  private int planNo; // fk from 일정
+  private int feedNo; // fk from 피드게시글
+
+
+  public int getMemberNo() {
+    return memberNo;
+  }
+
+  public void setMemberNo(int memberNo) {
+    this.memberNo = memberNo;
+  }
+
+  public int getNoticeBoardNo() {
+    return noticeBoardNo;
+  }
+
+  public void setNoticeBoardNo(int noticeBoardNo) {
+    this.noticeBoardNo = noticeBoardNo;
+  }
+
+  public int getPlanNo() {
+    return planNo;
+  }
+
+  public void setPlanNo(int planNo) {
+    this.planNo = planNo;
+  }
+
+  public int getFeedNo() {
+    return feedNo;
+  }
+
+  public void setFeedNo(int feedNo) {
+    this.feedNo = feedNo;
+  }
+}

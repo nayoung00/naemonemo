2cde1fe77fbc6aba54ccb3f786832d04f285e1af
sun-kyung <esun1801@naver.com>
2020-04-09 17:20:54 +0900

    [add] communication board dto 추가 dto 폴더에 CommunicationBoard, CommunicationBoardLike, CommunicationBoardReply, CommunicationBoardPhoto 클래스 추가

---
+++ src/main/java/com/nmnm/dto/CommunicationBoardReply.java
@@ -0,0 +1,42 @@
+package com.nmnm.dto;
+
+public class CommunicationBoardReply {
+  private int communicationReplyNo; // PK
+  private String content;
+  private int communicationBoardNo; // FK
+  private int memberNo; // FK
+
+
+  public int getCommunicationReplyNo() {
+    return communicationReplyNo;
+  }
+
+  public void setCommunicationReplyNo(int communicationReplyNo) {
+    this.communicationReplyNo = communicationReplyNo;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public int getCommunicationBoardNo() {
+    return communicationBoardNo;
+  }
+
+  public void setCommunicationBoardNo(int communicationBoardNo) {
+    this.communicationBoardNo = communicationBoardNo;
+  }
+
+  public int getMemberNo() {
+    return memberNo;
+  }
+
+  public void setMemberNo(int memberNo) {
+    this.memberNo = memberNo;
+  }
+
+}

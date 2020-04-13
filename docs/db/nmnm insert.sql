5aebd64105fb2ac3451a75c3d52f4443b33031e7
OhSeungWoo <fivewincow@gmail.com>
2020-04-13 12:44:11 +0900

    [add.mod] eXERD 및 .sql 수정
    nm_plan_member/ attend/ INTEGER -> VARCHAR(10)
    nm_member/ gender/ INTEGER -> VARCHAR(10)
    nm_member/ account_status -> default(1)
    nm_communication_board/ noticeable -> default(0)
    nm_normal_board, nm_normal_board_*/ -> nm_communication_board, nm_communication_board_*
    그외 자잘한 것들 수정사항 있으니 확인 후 작업

--- docs/db/nmnm insert.sql
+++ docs/db/nmnm insert.sql
@@ -55,10 +55,10 @@ insert into nm_grade(grade_no, grade_name)
 -- 사용권한 예제 (? 사용권한 번호랑 권한이랑 차이가 
 -- 뭔가요 굳이 둘다 번호로 나눌 필요가?) 아니면 VARCHAR를 INTEGER로 ??
 
-INSERT INTO nm_write_authority(authority_no,authority)
+INSERT INTO nm_write_authority(authority_no,authority_name)
   values(1,'관리자');
   
-INSERT INTO nm_write_authority(authority_no,authority)
+INSERT INTO nm_write_authority(authority_no,authority_name)
   values(2,'정회원');
   
 -- 회계권한 예제
@@ -72,7 +72,7 @@ INSERT INTO nm_notice_authority(notice_authority_no,authority_no,grade_no,group_
   values(1,1,1,1);
 
 INSERT INTO nm_notice_authority(notice_authority_no,authority_no,grade_no,group_no)
-  values(1,1,2,1);
+  values(2,1,2,1);
   
 -- 회원 예제 데이터
 
@@ -87,20 +87,18 @@ password,
 intro,      
 interest,     
 nickname,      
-account_status,
 withdraw,
 sns) 
 values('오승우',
 'abc.jpg',
 '1998-10-20',
-true,
+'남자',
 'fivewincow@gmail.com',
 '010-3002-0169',
 '1111',
 '열심히',
 6,
-'오승우',
-1,1,
+'오승우',1,
 '페이스북');
 
 INSERT INTO nm_member
@@ -114,20 +112,18 @@ password,
 intro,      
 interest,     
 nickname,      
-account_status,
 withdraw,
 sns) 
 values('손진영',
 'a.jpg',
 '1993-11-11',
-true,
+'남자',
 'mrHand@gmail.com',
 '010-2222-0119',
 '1111',
 '열심히',
 6,
-'손타쿠',
-1,1,
+'손타쿠',1,
 '인스타그램');
 
 INSERT INTO nm_member
@@ -141,20 +137,18 @@ password,
 intro,      
 interest,     
 nickname,      
-account_status,
 withdraw,
 sns) 
 values('신나라', 
 'photo.jpg', 
 '1988-4-9', 
-true, 
+'여자', 
 'test@gmail.com', 
 '010-6823-5479', 
 '1111', 
 '곰비둘기입니다.', 
 1, 
-'곰비둘기', 
-1, 1, 
+'곰비둘기',  1, 
 'instagram.com/gramrabbitbear');
  
 INSERT INTO nm_member
@@ -168,20 +162,18 @@ password,
 intro,      
 interest,     
 nickname,      
-account_status,
 withdraw,
 sns) 
 values('김나영', 
 'photo1.jpg', 
 '2000-4-9', 
-true, 
+'여자', 
 'test1@gmail.com', 
 '010-1111-1111', 
 '2222', 
 '김나영입니다.', 
 1, 
-'나사김', 
-1, 1, 
+'나사김',  1, 
 'instagram.com/test');
 
 
@@ -196,20 +188,18 @@ password,
 intro,      
 interest,     
 nickname,      
-account_status,
 withdraw,
 sns) 
 values('이선경', 
 'photo3.jpg', 
 '2000-2-2', 
-true, 
+'여자', 
 'test3@gmail.com', 
 '010-1113-1111', 
 '4444', 
 '이선경입니다.', 
 1, 
-'선경삼', 
-1, 1, 
+'선경삼',  1, 
 'instagram.com/test4');
 
   
@@ -259,27 +249,32 @@ INSERT INTO nm_notice_board(group_no,member_no,title,content)
 -- 여기 auto_increment 뭔가 이상함
 -- 사진인데 notice_board_no 이걸 auto_increment가 되어있음
  
-INSERT INTO nm_notice_photo(board_photo_no,photo_file,notice_board_no)
+INSERT INTO nm_notice_photo(notice_photo_no,photo_file,notice_board_no)
   values(1,'회비 납부하는법.jpg',1);
   
 -- 공지사항 댓글 예제
-
-INSERT INTO nm_reply(content,notice_board_no,member_no)
+INSERT INTO nm_notice_reply(content,notice_board_no,member_no)
   values('글이 성의 없네요 ㅡ.ㅡ',1,2);
-
+  
 -- 일정 예제 데이터
 
-insert into nm_plan
-(group_no,plan_date,title,place_name,address,latitude,longitude)
-values(1,'2020-04-09','나라누나 생축ㅊㅊㅊ','비트캠프 강남센터','ㅂ',11,22);
+INSERT INTO nm_plan
+(group_no,
+plan_date,
+title,
+place_name,
+address,
+latitude,
+longitude)
+VALUES(1,'2020-04-09','프로젝트','비트캠프 강남센터','주소',11,22);
 
 -- 일정 참여자 & 일정이 먼저
 
-INSERT INTO nm_plan_member(plan_board_no,member_no,group_no)
- values(1,1,1);
+INSERT INTO nm_plan_member(plan_no,member_no,group_no,attend)
+ values(1,1,1,'참가');
  
-INSERT INTO nm_plan_member(plan_board_no,member_no,group_no)
- values(1,2,1);
+INSERT INTO nm_plan_member(plan_no,member_no,group_no,attend)
+ values(1,2,1,'불참');
 
 -- 피드 게시글 예제
 
@@ -291,13 +286,13 @@ INSERT INTO nm_feed(member_no,group_no,title,content)
   
 -- 게시물 사진(피드(?)) 예제
 
-INSERT INTO nm_board_photo(photo_file,feed_no)
+INSERT INTO nm_feed_photo(photo_file,feed_no)
  values('생일파티.jpg',1);
  
-INSERT INTO nm_board_photo(photo_file,feed_no)
+INSERT INTO nm_feed_photo(photo_file,feed_no)
  values('생일파티2.jpg',1);
  
-INSERT INTO nm_board_photo(photo_file,feed_no)
+INSERT INTO nm_feed_photo(photo_file,feed_no)
  values('저번모임.jpg',2);
   
 -- 피드게시글 댓글 예제
@@ -310,34 +305,34 @@ INSERT INTO nm_feed_reply(content,feed_no,member_no)
   
 -- 소통 게시판 예제
 
-INSERT INTO nm_communication_board(normal_board_no,member_no,title,content,noticeable)
- values(1,1,'테스트 게시물입니다.','테스트 게시물입니다.',1);
+INSERT INTO nm_communication_board(member_no,title,content)
+ values(1,'테스트 게시물입니다.','테스트 게시물입니다.');
  
-INSERT INTO nm_communication_board(normal_board_no,member_no,title,content,noticeable)
- values(2,2,'내일 시간 되시는분','댓글 남겨주세요',0);
+INSERT INTO nm_communication_board(member_no,title,content)
+ values(2,'내일 시간 되시는분','댓글 남겨주세요');
  
-INSERT INTO nm_communication_board(normal_board_no,member_no,title,content,noticeable)
- values(3,3,'저희 회비 얼마인가요?','이번에 들어와서 모르겠네요',0);
+INSERT INTO nm_communication_board(member_no,title,content)
+ values(2,'저희 회비 얼마인가요?','이번에 들어와서 모르겠네요');
  
--- 댓글 예제 데이터 meeting_board_no
+-- 소통 게시판 댓글 예제
 
-insert into nm_communication_reply(content,normal_board_no,member_no)
+insert into nm_communication_reply(content,communication_board_no,member_no)
 values('무슨 테스트인가요?',1,1);
 
-insert into nm_communication_reply(content,normal_board_no,member_no)
+insert into nm_communication_reply(content,communication_board_no,member_no)
 values('저요~',2,1);
 
-insert into nm_communication_reply(content,normal_board_no,member_no)
-values('30000원 입니다',3,2);
+insert into nm_communication_reply(content,communication_board_no,member_no)
+values('30000원 입니다',2,2);
  
 -- 소통 게시판 사진 예제
 
-INSERT INTO nm_communication_photo(photo_file,normal_board_no)
+INSERT INTO nm_communication_photo(photo_file,communication_board_no)
   values('party.jpg',1);
   
 -- 소통게시판 좋아요(추천(?))
 
-INSERT INTO nm_normal_board_like(member_no,normal_board_no)
+INSERT INTO nm_communication_board_like(member_no,communication_board_no)
   values(1,1);
   
  -- 회계 유형
@@ -368,10 +363,10 @@ receipt_photo)
  
 -- 알림 예제
 
-INSERT INTO nm_alram(member_no,notice_board_no,plan_board_no,feed_no)
+INSERT INTO nm_alram(member_no,notice_board_no,plan_no,feed_no)
   values(1,1,1,1);
   
-INSERT INTO nm_alram(member_no,notice_board_no,plan_board_no,feed_no)
+INSERT INTO nm_alram(member_no,notice_board_no,plan_no,feed_no)
   values(2,1,1,1); 
 
 -- 알림은 4개중 하나만 올 수 있으니 not null 말고 0이랑 1로 구분하는게 좋을거 같아요
@@ -379,5 +374,5 @@ INSERT INTO nm_alram(member_no,notice_board_no,plan_board_no,feed_no)
  
 -- 소통 게시판 좋아요 예제
 
-INSERT INTO nm_normal_board_like(member_no,normal_board_no)
+INSERT INTO nm_communication_board_like(member_no,communication_board_no)
  values(1,2);

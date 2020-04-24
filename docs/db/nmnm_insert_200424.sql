a5af92f976374434cda9f11e19689364796ea3a1
nayoung00 <invin1201@gmail.com>
2020-04-24 16:03:47 +0900

    [mod] planMapper.xml 수정

--- docs/db/nmnm_insert_200424.sql
+++ docs/db/nmnm_insert_200424.sql
@@ -1,6 +1,4 @@
-DROP DATABASE nmnmdb;
-CREATE DATABASE nmnmdb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-use nmnmdb;
+
 
 -- 모임 예제
 INSERT INTO nm_group
@@ -256,13 +254,14 @@ INSERT INTO nm_plan
 (group_no,
 plan_date,
 title,
+subtitle,
 content,
 thumbnail,
 place_name,
 address,
 latitude,
 longitude)
-VALUES(1,'2020-04-09','프로젝트', '그룹프로젝트입니다', 'bit.jpg', '비트캠프 강남센터','주소',11,22);
+VALUES(1,'2020-04-09','프로젝트', '그룹프로젝트입니다','내용이랍니다', 'bit.jpg', '비트캠프 강남센터','주소',11,22);
 
 -- 일정 참여자 & 일정이 먼저
 

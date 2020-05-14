15d99b1c130380bc1bb6146fd3eb9c2520d50d3d
gomgugu <narashin547@gmail.com>
2020-05-14 14:29:50 +0900

    [mod] notice CRUD 수정

--- src/main/java/com/nmnm/gms/domain/Feed.java
+++ src/main/java/com/nmnm/gms/domain/Feed.java
@@ -11,78 +11,85 @@ public class Feed {
   private String content; // text NN
   private Date createDate; // DATETIME default=now() NN
   private int viewCount;
-  private String thumbnail;
-
+  
+  
   @Override
   public String toString() {
     return "Feed [feedNo=" + feedNo + ", memberNo=" + memberNo + ", groupNo=" + groupNo + ", title="
         + title + ", content=" + content + ", createDate=" + createDate + ", viewCount=" + viewCount
-        + ", thumbnail=" + thumbnail + "]";
+        + "]";
   }
+
 
   public int getFeedNo() {
     return feedNo;
   }
 
+
   public void setFeedNo(int feedNo) {
     this.feedNo = feedNo;
   }
+
 
   public int getMemberNo() {
     return memberNo;
   }
 
+
   public void setMemberNo(int memberNo) {
     this.memberNo = memberNo;
   }
+
 
   public int getGroupNo() {
     return groupNo;
   }
 
+
   public void setGroupNo(int groupNo) {
     this.groupNo = groupNo;
   }
+
 
   public String getTitle() {
     return title;
   }
 
+
   public void setTitle(String title) {
     this.title = title;
   }
+
 
   public String getContent() {
     return content;
   }
 
+
   public void setContent(String content) {
     this.content = content;
   }
+
 
   public Date getCreateDate() {
     return createDate;
   }
 
+
   public void setCreateDate(Date createDate) {
     this.createDate = createDate;
   }
+
 
   public int getViewCount() {
     return viewCount;
   }
 
+
   public void setViewCount(int viewCount) {
     this.viewCount = viewCount;
   }
 
-  public String getThumbnail() {
-    return thumbnail;
-  }
-
-  public void setThumbnail(String thumbnail) {
-    this.thumbnail = thumbnail;
-  }
 
 
 }

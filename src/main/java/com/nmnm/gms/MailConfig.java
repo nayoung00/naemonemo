760f78a5361994b7e59cc815600299ed80e105aa
nayoung00 <invin1201@gmail.com>
2020-05-15 13:17:45 +0900

    [add] 회원가입 이메일 인증 추가
     수정함:        src/main/java/com/nmnm/gms/MailConfig.java
     수정함:        src/main/java/com/nmnm/gms/service/MailSendService.java
     수정함:        src/main/java/com/nmnm/gms/service/impl/MailSendServiceImpl.java
     수정함:        src/main/java/com/nmnm/gms/service/impl/MemberServiceImpl.java
     수정함:        src/main/java/com/nmnm/gms/web/AuthController.java

--- src/main/java/com/nmnm/gms/MailConfig.java
+++ src/main/java/com/nmnm/gms/MailConfig.java
@@ -1,40 +1,39 @@
-// package com.nmnm.gms;
-//
-// import java.io.IOException;
-// import java.util.Properties;
-// import org.apache.logging.log4j.LogManager;
-// import org.apache.logging.log4j.Logger;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.mail.javamail.JavaMailSender;
-// import org.springframework.mail.javamail.JavaMailSenderImpl;
-//
-// @Configuration
-// public class MailConfig {
-//
-// static Logger logger = LogManager.getLogger(MailConfig.class);
-//
-// public MailConfig() throws IOException {
-// MailConfig.logger.info("[MailConfig.java] :: constructor called");
-// }
-//
-//
-// @Bean
-// public JavaMailSender mailSender() {
-// JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-// Properties mailProperties = new Properties();
-// mailProperties.put("mail.transport.protocol", "smtp");
-// mailProperties.put("mail.smtp.auth", "true");
-// mailProperties.put("mail.smtp.starttls.enable", "true");
-// mailProperties.put("mail.smtp.debug", "true");
-//
-// mailSender.setJavaMailProperties(mailProperties);
-// mailSender.setHost("smtp.gmail.com");
-// mailSender.setPort(587);
-// mailSender.setUsername("nemonaemo0@gmail.com");
-// mailSender.setPassword("nmnm5315");
-// mailSender.setDefaultEncoding("utf-8");
-// return mailSender;
-// }
-//
-// }
+package com.nmnm.gms;
+
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+  static Logger logger = LogManager.getLogger(MailConfig.class);
+
+  public MailConfig() throws IOException {
+    MailConfig.logger.info("[MailConfig.java] :: constructor called");
+  }
+
+
+  @Bean
+  public JavaMailSender mailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    Properties mailProperties = new Properties();
+    mailProperties.put("mail.transport.protocol", "smtp");
+    mailProperties.put("mail.smtp.auth", "true");
+    mailProperties.put("mail.smtp.starttls.enable", "true");
+    mailProperties.put("mail.smtp.debug", "true");
+
+    mailSender.setJavaMailProperties(mailProperties);
+    mailSender.setHost("smtp.gmail.com");
+    mailSender.setPort(587);
+    mailSender.setUsername("nemonaemo0@gmail.com");
+    mailSender.setPassword("nmnm5315");
+    mailSender.setDefaultEncoding("utf-8");
+    return mailSender;
+  }
+}
--- src/main/java/com/nmnm/gms/web/AuthController.java
+++ src/main/java/com/nmnm/gms/web/AuthController.java
@@ -1,139 +1,138 @@
-// package com.nmnm.gms.web;
-//
-//
-// import java.io.File;
-// import java.util.UUID;
-// import javax.servlet.ServletContext;
-// import javax.servlet.http.Cookie;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
-// import javax.servlet.http.HttpSession;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.ResponseBody;
-// import org.springframework.web.multipart.MultipartFile;
-// import com.nmnm.gms.domain.Member;
-// import com.nmnm.gms.service.MailSendService;
-// import com.nmnm.gms.service.MemberService;
-//
-// @Controller
-// @RequestMapping("/auth")
-// public class AuthController {
-//
-// @Autowired
-// ServletContext servletContext;
-//
-// @Autowired
-// MemberService memberService;
-//
-// @Autowired
-// private MailSendService mailsender;
-//
-//
-// @GetMapping("login")
-// public void login() {}
-//
-// @GetMapping("join")
-// public void join() {}
-//
-//
-// @PostMapping("join")
-// public String join(Member member, MultipartFile photoFile, HttpServletRequest request,
-// Model model) throws Exception {
-// if (photoFile.getSize() > 0) {
-// String dirPath = servletContext.getRealPath("/upload/member");
-// String filename = UUID.randomUUID().toString();
-// photoFile.transferTo(new File(dirPath + "/" + filename));
-// member.setPhoto(filename);
-// }
-//
-// if (memberService.join(member) > 0) {
-// // 인증 메일 보내기 메서드
-// mailsender.mailSendWithKey(member.getEmail(), member.getName(), request);
-// return "redirect:../../index.html";
-// } else {
-// throw new Exception("회원을 추가할 수 없습니다.");
-// }
-// }
-//
-// @GetMapping("regSuccess")
-// public void regSuccess() {}
-//
-//
-// @GetMapping(value = "keyalter")
-// public String keyalterConfirm(@RequestParam("email") String email,
-// @RequestParam("key") String key) {
-//
-// mailsender.alterUserKey(email, key); // mailsender의 경우 @Autowired
-//
-// return "redirect:regSuccess";
-// }
-//
-// @ResponseBody
-// @RequestMapping(value = "checkid", method = RequestMethod.POST)
-// public int checkid(String email) throws Exception {
-// int count = memberService.checkid(email);
-// return count;
-// }
-//
-// @ResponseBody
-// @RequestMapping(value = "checknick", method = RequestMethod.POST)
-// public int checknick(String nickname) throws Exception {
-// System.out.println(nickname);
-// int count = memberService.checknick(nickname);
-// return count;
-// }
-//
-// @PostMapping("login")
-// public String login(String email, String password, String saveEmail, HttpServletResponse
-// response,
-// HttpSession session, Model model) throws Exception {
-// Cookie cookie = new Cookie("email", email);
-// if (saveEmail != null) {
-// cookie.setMaxAge(60 * 60 * 24 * 30);
-// } else {
-// cookie.setMaxAge(0);
-// }
-// response.addCookie(cookie);
-//
-// Member member = memberService.get(email, password);
-// if (member != null) {
-// session.setAttribute("loginUser", member);
-// model.addAttribute("refreshUrl", "2;url=../../index.html");
-// } else {
-// session.invalidate();
-// model.addAttribute("refreshUrl", "2;url=login");
-// }
-//
-// return "auth/loginForm";
-// }
-//
-// @GetMapping("logout")
-// public String logout(HttpSession session) {
-// session.invalidate();
-// return "redirect:../../index.html";
-// }
-//
-// // @GetMapping("findPassword")
-// // public void findPasswordForm() {}
-// //
-// // @PostMapping("findPassword")
-// // public String findPassword(String email, Model model) throws Exception {
-// // String memberEmail = memberService.getEmailByEmail(email);
-// //
-// // if (memberEmail != null) {
-// // model.addAttribute("email", email);
-// // mailsender.findPassword(email);
-// // return "redirect:/";
-// // } else {
-// // model.addAttribute("error", "해당 이메일은 가입된 이메일이 아닙니다.");
-// // return "redirect:./";
-// // }
-// // }
-// }
+package com.nmnm.gms.web;
+
+
+import java.io.File;
+import java.util.UUID;
+import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import com.nmnm.gms.domain.Member;
+import com.nmnm.gms.service.MailSendService;
+import com.nmnm.gms.service.MemberService;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+  @Autowired
+  ServletContext servletContext;
+
+  @Autowired
+  MemberService memberService;
+
+  @Autowired
+  private MailSendService mailsender;
+
+
+  @GetMapping("login")
+  public void login() {}
+
+  @GetMapping("join")
+  public void join() {}
+
+
+  @PostMapping("join")
+  public String join(Member member, MultipartFile photoFile, HttpServletRequest request,
+      Model model) throws Exception {
+    if (photoFile.getSize() > 0) {
+      String dirPath = servletContext.getRealPath("/upload/member");
+      String filename = UUID.randomUUID().toString();
+      photoFile.transferTo(new File(dirPath + "/" + filename));
+      member.setPhoto(filename);
+    }
+
+    if (memberService.join(member) > 0) {
+      // 인증 메일 보내기 메서드
+      mailsender.mailSendWithKey(member.getEmail(), member.getName(), request);
+      return "redirect:../../index.html";
+    } else {
+      throw new Exception("회원을 추가할 수 없습니다.");
+    }
+  }
+
+  @GetMapping("regSuccess")
+  public void regSuccess() {}
+
+
+  @GetMapping(value = "keyalter")
+  public String keyalterConfirm(@RequestParam("email") String email,
+      @RequestParam("key") String key) {
+
+    mailsender.alterUserKey(email, key); // mailsender의 경우 @Autowired
+
+    return "redirect:regSuccess";
+  }
+
+  @ResponseBody
+  @RequestMapping(value = "checkid", method = RequestMethod.POST)
+  public int checkid(String email) throws Exception {
+    int count = memberService.checkid(email);
+    return count;
+  }
+
+  @ResponseBody
+  @RequestMapping(value = "checknick", method = RequestMethod.POST)
+  public int checknick(String nickname) throws Exception {
+    System.out.println(nickname);
+    int count = memberService.checknick(nickname);
+    return count;
+  }
+
+  @PostMapping("login")
+  public String login(String email, String password, String saveEmail, HttpServletResponse response,
+      HttpSession session, Model model) throws Exception {
+    Cookie cookie = new Cookie("email", email);
+    if (saveEmail != null) {
+      cookie.setMaxAge(60 * 60 * 24 * 30);
+    } else {
+      cookie.setMaxAge(0);
+    }
+    response.addCookie(cookie);
+
+    Member member = memberService.get(email, password);
+    if (member != null) {
+      session.setAttribute("loginUser", member);
+      model.addAttribute("refreshUrl", "2;url=../../index.html");
+    } else {
+      session.invalidate();
+      model.addAttribute("refreshUrl", "2;url=login");
+    }
+
+    return "auth/loginForm";
+  }
+
+  @GetMapping("logout")
+  public String logout(HttpSession session) {
+    session.invalidate();
+    return "redirect:../../index.html";
+  }
+
+  // @GetMapping("findPassword")
+  // public void findPasswordForm() {}
+  //
+  // @PostMapping("findPassword")
+  // public String findPassword(String email, Model model) throws Exception {
+  // String memberEmail = memberService.getEmailByEmail(email);
+  //
+  // if (memberEmail != null) {
+  // model.addAttribute("email", email);
+  // mailsender.findPassword(email);
+  // return "redirect:/";
+  // } else {
+  // model.addAttribute("error", "해당 이메일은 가입된 이메일이 아닙니다.");
+  // return "redirect:./";
+  // }
+  // }
+}
